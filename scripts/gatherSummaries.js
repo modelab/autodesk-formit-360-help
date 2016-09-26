f80aec8a9a6581c0815cac969210d3f46b0fcd5c
Robert Wells <rwells@modelab.is>
2016-09-26 11:21:37 -0400

    build the book

--- scripts/gatherSummaries.js
+++ scripts/gatherSummaries.js
@@ -2,9 +2,11 @@ var readline = require('readline');
 var exec = require('child_process').exec;
 var fs = require('fs');
 
+const ENCODING = 'utf8';
+
 // Revert all local changes 
 // so that we can append the sub-summary files to the root summary file
-exec('git reset --hard HEAD');
+//exec('git reset --hard HEAD');
 
 var rootDir = __dirname + '/../';
 var summaryFile = '/SUMMARY.md';
@@ -19,7 +21,7 @@ exec('git submodule init && git submodule update');
 
 // Parse paths from .gitmodules file
 try {
-	var gitmodules = fs.readFileSync(rootDir + submodulesFile, 'utf8');
+	var gitmodules = fs.readFileSync(rootDir + submodulesFile, ENCODING);
 	var submodules = gitmodules.match(submoduleRegex);
 	var paths = [];
 	for (var i = 0; i < submodules.length; i++) {
@@ -32,24 +34,26 @@ try {
 var titleRegex = /#\ [\w\ ]+/g;
 var poundRegex = /#\ /g;
 var blanklineRegex = /^\s*[\r\n]/gm
-var dashRegex = /\-/g;
-var starRegex = /\*/g;
+var dashRegex = /\-\ /g;
+var starRegex = /\*\ /g;
 
 // Parse each submodule's summary file and append to root summary file
-for (var i = 0; i < submodules.length; i++) {
+for (var i = 0; i < paths.length; i++) {
 	try {
-		var submoduleSummaryFile = rootDir + submodules[i] + summaryFile;
+		var submoduleSummaryFile = rootDir + paths[i] + summaryFile;
 		if (fs.statSync(submoduleSummaryFile)) {
-			var summary = fs.readFileSync(submoduleSummaryFile);
+			var summary = fs.readFileSync(submoduleSummaryFile, ENCODING);
+			console.log(summary);
 			summary = summary.replace(titleRegex, function(s) {
 				s = s.replace(poundRegex, '- [');
-				s = s + '](' + submodules[i] + readmeFile + ')';
+				s = s + '](' + paths[i] + readmeFile + ')';
+				console.log(s);
 				return s;
 			});
 			summary = summary.replace(blanklineRegex, '');
-			summary = summary.replace(dashRegex, '\t-');
-			summary = summary.replace(starRegex, '\t*');
-			fs.fileAppendSync(rootDir + 'SUMMARY.md', summary);
+			summary = summary.replace(dashRegex, '\t- ');
+			summary = summary.replace(starRegex, '\t* ');
+			fs.appendFileSync(rootDir + 'SUMMARY.md', summary);
 		}
 	} catch(err) {
 		console.log(err);
