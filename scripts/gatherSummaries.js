cbe064902d2bb5a7801aab5020fa174180317825
Robert Wells <rwells@modelab.is>
2016-09-26 18:15:24 -0400

    remove the s

--- scripts/gatherSummaries.js
+++ scripts/gatherSummaries.js
@@ -39,11 +39,11 @@ for (var i = 0; i < books.length; i++) {
 		if (fs.statSync(bookSummaryFile)) {
 			var summary = fs.readFileSync(bookSummaryFile, ENCODING);
 			summary = summary.replace(blanklineRegex, '\n')
-							 .replace(dashRegex, '\s\s- ')
-							 .replace(starRegex, '\s\s* ')
+							 .replace(dashRegex, '  - ')
+							 .replace(starRegex, '  * ')
 							 .replace(linkRegex, '](' + booksDir + books[i] + '/')
 							 .replace(titleRegex, function(s) {
-								s = s.replace(poundRegex, '\s\s- [');
+								s = s.replace(poundRegex, '  - [');
 								s = s + '](' + booksDir + books[i] + '/README.md' + ')';
 								return s;
 							 });
