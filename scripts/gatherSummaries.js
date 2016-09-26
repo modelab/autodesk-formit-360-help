eb2825379dfd54ee287bc1332ebe399c89f9570f
Robert Wells <rwells@modelab.is>
2016-09-26 14:13:11 -0400

    refactor script to parse books directory

--- scripts/gatherSummaries.js
+++ scripts/gatherSummaries.js
@@ -11,21 +11,13 @@ var rootDir = __dirname + '/../';
 var summaryFile = '/SUMMARY.md';
 var readmeFile = '/README.md';
 
-var submodulesFile = '.gitmodules';
-var submoduleRegex = /\[submodule [\w/\-"._]+\][\s]+path\s=\s([\w\-. /]+)/g;
-var pathRegex = /[submodule [\w/\-".]+\][\s]+path\s=\s/g
+var booksDir = 'books/';
 
-// Parse paths from .gitmodules file
-try {
-	var gitmodules = fs.readFileSync(rootDir + submodulesFile, ENCODING);
-	var submodules = gitmodules.match(submoduleRegex);
-	var paths = [];
-	for (var i = 0; i < submodules.length; i++) {
-		paths.push(submodules[i].replace(pathRegex, ''));
-	}
-} catch(err) {
-	console.log(err);
-}
+var books = fs.readdirSync(rootDir + booksDir).filter(function(file) {
+	return fs.statSync(path.join(rootDir + booksDir, file)).isDirectory();
+});
+
+console.log(books);
 
 var titleRegex = /#\ [\w\ ]+/g;
 var poundRegex = /#\ /g;
@@ -33,24 +25,24 @@ var blanklineRegex = /^\s*[\r\n]/gm
 var dashRegex = /\-\ /g;
 var starRegex = /\*\ /g;
 
-// Parse each submodule's summary file and append to root summary file
-for (var i = 0; i < paths.length; i++) {
-	try {
-		var submoduleSummaryFile = rootDir + paths[i] + summaryFile;
-		if (fs.statSync(submoduleSummaryFile)) {
-			var summary = fs.readFileSync(submoduleSummaryFile, ENCODING);
-			summary = summary.replace(titleRegex, function(s) {
-				s = s.replace(poundRegex, '- [');
-				s = s + '](' + paths[i] + readmeFile + ')';
-				console.log(s);
-				return s;
-			});
-			summary = summary.replace(blanklineRegex, '');
-			summary = summary.replace(dashRegex, '\t- ');
-			summary = summary.replace(starRegex, '\t* ');
-			fs.appendFileSync(rootDir + 'SUMMARY.md', summary);
-		}
-	} catch(err) {
-		console.log(err);
-	}
-}
+// // Parse each submodule's summary file and append to root summary file
+// for (var i = 0; i < paths.length; i++) {
+// 	try {
+// 		var submoduleSummaryFile = rootDir + paths[i] + summaryFile;
+// 		if (fs.statSync(submoduleSummaryFile)) {
+// 			var summary = fs.readFileSync(submoduleSummaryFile, ENCODING);
+// 			summary = summary.replace(titleRegex, function(s) {
+// 				s = s.replace(poundRegex, '- [');
+// 				s = s + '](' + paths[i] + readmeFile + ')';
+// 				console.log(s);
+// 				return s;
+// 			});
+// 			summary = summary.replace(blanklineRegex, '');
+// 			summary = summary.replace(dashRegex, '\t- ');
+// 			summary = summary.replace(starRegex, '\t* ');
+// 			fs.appendFileSync(rootDir + 'SUMMARY.md', summary);
+// 		}
+// 	} catch(err) {
+// 		console.log(err);
+// 	}
+// }
